8869ba32ffee800ff3767a458734b53e1c0f0888
Vinay Hiremath <vhiremath4@gmail.com>
2024-06-24 18:36:09 -0400

    model module skeletons based on GPT-2 and GPT-1 papers

--- model.py
+++ model.py
@@ -10,6 +10,65 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 
+class LayerNorm(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        """
+        same as usual causal self-attention but with:
+        - attention Dropout
+        - Linear projection for the residual network
+        - residual dropout
+        """
+
+    def forward(self, x):
+        return x
+
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        """"
+        [
+            # layers for the MLP
+            Linear,
+            GELU,
+
+            # residual projection layer
+            Linear,
+
+            # dropout for the residual layer (in GPT-1 paper)
+            Dropout
+        ]
+        additional for residual network: [projection,]
+        Linear projection (residual network) -> Linear (learning the )
+        """
+        self.config = config
+
+    def forward(self, x):
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        """
+        GPT-1:
+        [masked self-attention, layernorm, feed forward, layernorm]
+
+        with GPT-2 modifications from paper:
+        [layernorm, masked self-attention, layernorm, feed forward]
+        """
+        self.config = config
+
+    def forward(self, x):
+        return x
+
+
 # largely copied from nanogpt
 @dataclass
 class GPTConfig:
@@ -21,7 +80,7 @@ class GPTConfig:
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
-class GPT(nn.module):
+class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
