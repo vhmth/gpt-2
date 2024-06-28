c2144fd32618fe4bcf4e14df3f63cf4cd0849b0a
Vinay Hiremath <vhiremath4@gmail.com>
2024-06-28 14:25:30 -0400

    train skeleton

--- train.py
+++ train.py
@@ -16,3 +16,24 @@ TODOS:
 - log to wandb or neptune
 - cuda-ify everything
 """
+
+import torch
+from model import GPTConfig, GPT
+
+# hyperparameters
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+# -------------
+
+# 1. load the model (TODO: pull from checkpoint)
+# 2. create optimizer and establish learning loop
+# 3. forward: pull out minibatches of data
+# 4. zerograd
+# 5. train
+# 6. calculate loss
+# 7. update each parameter by the learning rate (TODO: get parameters as list from model)
