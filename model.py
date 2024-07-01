68ba73481fe6cd827eadda79e281a3ed0df07362
Vinay <vhiremath4@gmail.com>
2024-07-01 13:56:32 +0000

    nanogpt causalselfattention implementation for performance

--- model.py
+++ model.py
@@ -7,53 +7,64 @@ module and then used to train and sample from the trained model.
 
 from dataclasses import dataclass
 
+import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-# modified from previous lesson with bigram model
-class Head(nn.Module):
-    """ one head of self-attention """
-
-    def __init__(self, config, head_size):
-        super().__init__()
-        n_embd = config.n_embd
-        bias = config.bias
-        block_size = config.block_size
-
-        self.key = nn.Linear(n_embd, head_size, bias=bias)
-        self.query = nn.Linear(n_embd, head_size, bias=bias)
-        self.value = nn.Linear(n_embd, head_size, bias=bias)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
-        self.dropout = nn.Dropout(config.dropout)
-
-    def forward(self, x):
-        B,T,C = x.shape
-        k = self.key(x) # (B,T,C)
-        q = self.query(x) # (B,T,C)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B,T,C) @ (B,C,T) -> (B,T,T)
-        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # (B,T,T)
-        wei = F.softmax(wei, dim=-1) # (B,T,T)
-        wei = self.dropout(wei)
-        # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,C)
-        out = wei @ v
-        return out
-
+# Copied CausalSelfAttention from nanogpt for its speed. Got this working with
+# my previous bigram implementation from the course, but it was too slow since:
+#
+# 1. It used separate k,v,q tensors for each head instead of 1
+# 2. It die not take advantage of flash attention (scaled_dot_product_attention)
 class CausalSelfAttention(nn.Module):
+
     def __init__(self, config):
         super().__init__()
-        n_head = config.n_head
-        n_embd = config.n_embd
-        self.heads = nn.ModuleList([Head(config, n_embd // n_head) for _ in range(n_head)])
-        self.residual_proj = nn.Linear(n_embd, n_embd) # projection layer going back into the residual pathway
-        self.residual_dropout = nn.Dropout(config.dropout)
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.residual_dropout(self.residual_proj(out))
-        return x
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if self.flash:
+            # efficient attention using Flash Attention CUDA kernels
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
 
 class Block(nn.Module):
     def __init__(self, config):
