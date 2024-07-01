b80683a2b17f6737c148dd9a8154eddf22ad15b6
Vinay <vhiremath4@gmail.com>
2024-07-01 15:45:40 +0000

    WIP: sample from the model

---
+++ sample.py
@@ -0,0 +1,35 @@
+"""
+Sample from the model. If a trained model checkpoint exists, this script
+will automatically load it into the model.
+"""
+
+import os
+
+import tiktoken
+import torch
+
+from model import GPTConfig, GPT
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+gpt_config = GPTConfig(device=device)
+model = GPT(gpt_config)
+model.to(device)
+
+chkpt_file = os.path.join('out', 'chckpt.pt')
+
+checkpoint = None
+if os.path.exists(chkpt_file):
+    print(f"loading checkpoint from {chkpt_file}")
+    checkpoint = torch.load(chkpt_file)
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    curr_epoch = checkpoint['curr_epoch']
+    best_val_loss = checkpoint['best_val_loss']
+    print(f"loaded checkpoint, trained over {curr_epoch} epochs with best val loss: {best_val_loss}")
+else:
+    print("no checkpoint file found, loading untrained model")
+
+# generate from the model
+enc = tiktoken.get_encoding("gpt2")
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(enc.decode(model.generate(context, max_new_tokens=500)[0].tolist()))
